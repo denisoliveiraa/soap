2093622c4e172fe4582d1502ce690585ba395a9a
denisoliveiraa <denis_o@hotmail.com.br>
2024-05-28 22:57:06 -0300

    Addint setup for enviroment variables

---
+++ env/index.ts
@@ -0,0 +1,22 @@
+import 'dotenv/config'
+import {z} from 'zod'
+
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(['dev', 'test','production']).default('dev'),
+  PORT: z.coerce.number().default(4324)
+})
+
+const _env = envSchema.safeParse(process.env) //Valida se as variaveis ambiente "process.env" se tem exatamente as informações do envSchema
+//process.env ele busca do .env qual setup está sendo utilizado  {NODE_ENV: 'dev'}
+
+if(_env.success === false){
+  console.error('Invalid enviroment variable', _env.error.format())
+
+  throw new Error('Invalid enrinment variables.') // somente para derrubar a aplicação se tem problema na varivel ambiente
+}
+
+// Caso seja sucesso
+
+export const env = _env.data // data traz cada um dos valores da variável ambiente
+
---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { env } from "../env"
+import {app} from "./app"
+
+app.listen({
+  host: '0.0.0.0', // Aplicação acessivel front-end
+  port: env.PORT,
+}).then(() => {
+  console.log('Http Server is running')
+})
