ecc50621ec83f2e35aca7923a38a09fe091f9d6f
denisoliveiraa <denis_o@hotmail.com.br>
2024-06-19 23:02:28 -0300

    Adding validation for erros

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,24 @@
 import fastify from 'fastify'
 import { appRoutes } from './http/routes'
+import { ZodError } from 'zod'
+import { env } from './env'
 
 export const app = fastify()
 
 app.register(appRoutes)
+
+app.setErrorHandler((Error, _, reply) => {
+  if (Error instanceof ZodError) {
+    // zodError é o tipo de error que o zod envia
+    return reply
+      .status(400)
+      .send({ message: 'Validantion error', issues: Error.format() })
+  }
+  // eslint-disable-next-line eqeqeq
+  if (env.NODE_ENV != 'production') {
+    console.error(Error)
+  } else {
+    // TODO: Here we should log to an extenal toll like Datalog
+  }
+  return reply.status(500).send({ message: 'Internal Server error' })
+})
