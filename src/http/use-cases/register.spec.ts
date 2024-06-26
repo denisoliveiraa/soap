d354f4bb318b8da98bf2093c4acf3b9e055cdc64
denisoliveiraa <denis_o@hotmail.com.br>
2024-06-25 22:31:41 -0300

    NEw testing mapping for email register

--- src/http/use-cases/register.spec.ts
+++ src/http/use-cases/register.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { InMemoryUsersRepository } from '../repositories/in-memory-users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists'
 
 describe('Register use Case', () => {
   it('Should hash user password upon registration', async () => {
@@ -16,5 +17,26 @@ describe('Register use Case', () => {
 
     const isPasswordCorrectlyHased = await compare('123456', user.password_hash)
     expect(isPasswordCorrectlyHased).toBe(true)
+  })
+
+  it('Shoul bot be able to register with same email twice', async () => {
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    const email = 'jonhdoe@example.com'
+
+    await registerUseCase.execute({
+      name: 'Jonh Doe',
+      email,
+      password: '123456',
+    })
+
+    expect(() =>
+      registerUseCase.execute({
+        name: 'John Doe',
+        email,
+        password: '123456',
+      }),
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError)
   })
 })
