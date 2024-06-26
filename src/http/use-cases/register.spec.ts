b2d778e41a65fc785d5955f891c741c3d8631244
denisoliveiraa <denis_o@hotmail.com.br>
2024-06-25 22:25:07 -0300

    addint testing with unique repository

--- src/http/use-cases/register.spec.ts
+++ src/http/use-cases/register.spec.ts
@@ -1,18 +1,20 @@
-import { describe, it } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import { RegisterUseCase } from './register'
-import { PrismaUsersRepository } from '../repositories/prisma/prisma-users-repository'
+import { compare } from 'bcryptjs'
+import { InMemoryUsersRepository } from '../repositories/in-memory-users-repository'
 
 describe('Register use Case', () => {
   it('Should hash user password upon registration', async () => {
-    const primaUsersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(primaUsersRepository)
+    const usersRepository = new InMemoryUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
 
     const { user } = await registerUseCase.execute({
       name: 'John Doe',
-      email: 'johndoe@example.com',
+      email: 'johndoe11@example.com',
       password: '123456',
     })
 
-    console.log(user.password_hash)
+    const isPasswordCorrectlyHased = await compare('123456', user.password_hash)
+    expect(isPasswordCorrectlyHased).toBe(true)
   })
 })
