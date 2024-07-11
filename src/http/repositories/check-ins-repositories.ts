60e370e553cdeb91fa575563dc2c66a269f6fed3
denisoliveiraa <denis_o@hotmail.com.br>
2024-07-10 21:58:01 -0300

    Adding funtionality for get total Checkins

--- src/http/repositories/check-ins-repositories.ts
+++ src/http/repositories/check-ins-repositories.ts
@@ -4,4 +4,5 @@ export interface CheckInsRepository {
   findByUserIdOnDate(userId: string, arg1: Date): unknown
   create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn>
   findManyByUserId(userId: string, page: number): Promise<CheckIn[]>
+  countByUserId(userId: string): Promise<number>
 }
