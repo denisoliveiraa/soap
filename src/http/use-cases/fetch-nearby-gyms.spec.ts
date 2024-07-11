d2a803c1d1b7aedb3b8a3d7e2c9c897abd66ec1e
denisoliveiraa <denis_o@hotmail.com.br>
2024-07-10 23:49:31 -0300

    Adding test for search a gym distance 10km

---
+++ src/http/use-cases/fetch-nearby-gyms.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryGymsRepository } from '../repositories/in-memory/in-memory-gyms-repositories'
+import { expect, describe, it, beforeEach } from 'vitest'
+import { FetchNearbyGymsUseCase } from './fetch-nearby-gyms'
+
+let gymsRepository: InMemoryGymsRepository
+let sut: FetchNearbyGymsUseCase
+
+describe('fetch nearby gyms use case', () => {
+  beforeEach(async () => {
+    gymsRepository = new InMemoryGymsRepository()
+    sut = new FetchNearbyGymsUseCase(gymsRepository)
+  })
+
+  it('should be able to fetch nearby gyms', async () => {
+    await gymsRepository.create({
+      title: 'Near Gym',
+      description: null,
+      phone: null,
+      latitude: -27.20920528,
+      longitude: -49.5229501,
+    })
+
+    await gymsRepository.create({
+      title: 'Far gym',
+      description: null,
+      phone: null,
+      latitude: -27.2092052,
+      longitude: -49.6401091,
+    })
+
+    const { gyms } = await sut.execute({
+      userLatitude: -27.2092052,
+      userLongitude: -49.6401891,
+    })
+
+    expect(gyms).toHaveLength(1)
+    expect(gyms).toEqual([expect.objectContaining({ title: 'Near Gym' })])
+  })
+})
