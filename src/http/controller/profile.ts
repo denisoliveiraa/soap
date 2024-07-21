3d5f35544bbb426bb596953955b88a9a5d29c1ae
denisoliveiraa <denis_o@hotmail.com.br>
2024-07-21 20:35:38 -0300

    Adding the validate for user Login

--- src/http/controller/profile.ts
+++ src/http/controller/profile.ts
@@ -1,4 +1,6 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify() // busca o token no header e se realmente foi gerado pela applicação
+
   return reply.status(200).send()
 }
