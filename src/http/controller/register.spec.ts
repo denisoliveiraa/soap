d4fa06c4178745436fc0c480b2b922a3f7b1a2a5
denisoliveiraa <denis_o@hotmail.com.br>
2024-07-23 22:07:44 -0300

    e2e for register

--- src/http/controller/register.spec.ts
+++ src/http/controller/register.spec.ts
@@ -1,3 +1,22 @@
-import { test } from 'vitest'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import { app } from '@/app'
+import request from 'supertest'
 
-test('ok', () => {})
+describe('Register (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('Shoul be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Denis',
+      email: 'Denis@hotmal',
+      password: '12345',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
